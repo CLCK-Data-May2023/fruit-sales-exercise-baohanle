60399055ed2fc7c49a2042dc55e9aa02afc68a51
Han Le <baohan1114@gmail.com>
2023-08-23 19:16:35 -0400

    done

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,19 @@
-# add your code here
+# import dependencies
+import pandas as pd
+
+# create/import data
+
+# pd.DataFrame({'Apples': [35,41], 'Bananas': [21,34]}, index=['2017 Sales', '2018 Sales'])
+
+data = {
+    'Apples': [35,41],
+    'Bananas': [21,34]
+}
+
+df = pd.DataFrame(data, index = ['2017 Sales','2018 Sales'])
+
+print(df)
+df.to_csv('fruit.csv')
+# create dataframe from data
+
+# export data frame to csv
